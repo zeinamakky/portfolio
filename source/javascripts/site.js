6980aa00e5bba1585964654325b62eae8d3df386
Zeina <zeemakky@gmail.com>
2018-11-20 19:48:08 -0500

    page transitions working

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,5 +1,18 @@
 // This is where it all goes :)
 
-$(function() {
-  $('.container').smoothState();
-});
+;(function ($) {
+  'use strict';
+  var content  = $('.container').smoothState({
+        // onStart runs as soon as link has been activated
+        onStart : {
+          
+          
+          // Alterations to the page
+          render: function () {
+
+            // Quickly toggles a class and restarts css animations
+            content.toggleAnimationClass('is-exiting');
+          }
+        }
+      }).data('smoothState'); // makes public methods available
+})(jQuery);
